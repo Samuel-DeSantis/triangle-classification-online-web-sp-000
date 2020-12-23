46d2a9023ab300eb01efa6900db0ed3da8d1cd26
Samuel DeSantis <samuel.i.desantis@gmail.com>
2020-12-23 15:43:00 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,42 @@
 class Triangle
-  # write code here
+
+  def initialize(x, y, z)
+    @x = x
+    @y = y
+    @z = z
+  end
+
+  def kind
+    if @x == 0 || @y == 0 || @z == 0
+      raise TriangleError
+    elsif @x < 0 || @y < 0 || @z < 0
+      raise TriangleError
+    elsif tri_inequality?(@x, @y, @z)
+      raise TriangleError
+    elsif @x == @y && @y == @z
+      :equilateral
+    elsif @x == @y || @y == @z || @x == @z
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def tri_inequality?(a, b, c)
+    if a + b > c
+      next
+    elsif a + c > b
+      next
+    elsif b + c > a
+      next
+    else
+      false
+    end
+    true
+  end
+
+  class TriangleError < StandardError
+
+  end
+
 end
