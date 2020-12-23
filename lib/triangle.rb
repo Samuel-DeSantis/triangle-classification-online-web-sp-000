ccd78e348ca2c7ceec0e92e1dde07d532750d278
Samuel DeSantis <samuel.i.desantis@gmail.com>
2020-12-23 15:52:23 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,13 +7,7 @@ class Triangle
   end
 
   def kind
-    if @x == 0 || @y == 0 || @z == 0
-      raise TriangleError
-    elsif @x < 0 || @y < 0 || @z < 0
-      raise TriangleError
-#    elsif tri_inequality?(@x, @y, @z)
-#      raise TriangleError
-    elsif @x == @y && @y == @z
+    if @x == @y && @y == @z
       :equilateral
     elsif @x == @y || @y == @z || @x == @z
       :isosceles
@@ -22,17 +16,12 @@ class Triangle
     end
   end
 
-  def tri_inequality?(a, b, c)
-    if a + b > c
-      true
-    elsif a + c > b
-      true
-    elsif b + c > a
-      true
-    else
-      false
+  def validate_triangle
+    real_triangle = [(a + b > c), (a + c > b), (b + c > a)]
+    [a, b, c].each do |side|
+      real_triangle << false if side <= 0
+      raise TriangleError if real_triangle.include?(false)
     end
-    true
   end
 
   class TriangleError < StandardError
